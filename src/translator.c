da3346ffadc8baa9bc58168c70574bc52f2ab11a
mouse_greys <greys.mouse@yandex.ru>
2026-03-30 16:28:58 +0300

    translator development

--- src/translator.c
+++ src/translator.c
@@ -15,6 +15,28 @@ char *translate(FILE *src, long src_len)
     char *result;
 
     tokens = scan(src, src_len);
+    result = create_buf(sizeof(char) * (src_len + 1));
+
+    fact(&tokens, &result);
 
     return result;
+}
+
+static void fact(token **tokens, char **result)
+{
+    token_id id = (**tokens).id;
+
+    if (id == NUM) {
+        // memcpy();
+        *tokens++;
+        *result += 2;
+    } else if (peek == '(') {
+        fact(input, result, ppos);
+        expr(input, result, 1, ppos);
+        rpar(input, result, ppos);
+    } else if (peek == EOF) {
+        SYNTAX_NO_OPND_ERR(*ppos + 1);
+    } else {
+        SYNTAX_INVAL_OPND_ERR(*ppos, peek);
+    }
 }
